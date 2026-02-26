790c0a87fdb8e6d83c922a7220ca2c7ec2d423b6
Marissa Rico <marissa.rico28@gmail.com>
2026-02-26 17:48:09 +0100

    refactor: clean responsive layout, fix bounce and restore hero hover

--- js/app.js
+++ js/app.js
@@ -20,7 +20,7 @@ function generatePoem(event) {
 
   let poemElement = document.querySelector("#poem");
   poemElement.classList.remove("hidden");
-  poemElement.innerHTML = `<div class= "generating"> ⏳Generating the Mexican Poem about ${instructionsInput.value} </div>`;
+  poemElement.innerHTML = `<div class= "generating"> ⏳Generating the Mexican Poem in Spanish about ${instructionsInput.value} </div>`;
 
   axios.get(apiURL).then(displayPoem);
 }
